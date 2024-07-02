5aada6a875b89ba69313a334f33942d2a94844ee
Mirko Abozzi <mirko.abozzi@gmail.com>
2024-07-02 14:26:10 +0200

    refactor CommentArea

--- src/components/CommentArea.jsx
+++ src/components/CommentArea.jsx
@@ -1,26 +1,29 @@
-import { Component } from "react";
 import CommentList from "./CommentList";
 import AddComment from "./AddComment";
 import { Alert } from "react-bootstrap";
+import { useEffect, useState } from "react";
 
-class CommentArea extends Component {
-  state = {
-    selected: false,
-    comments: [],
-  };
+const CommentArea = (props) => {
+  // state = {
+  //   selected: false,
+  //   comments: [],
+  // };
 
-  fetchComments = async () => {
-    this.setState({ selected: true });
+  const [selected, setSelected] = useState(false);
+  const [comments, setComments] = useState([]);
+
+  const fetchComments = async () => {
+    setSelected(true);
     try {
-      const response = await fetch("https://striveschool-api.herokuapp.com/api/comments/" + this.props.asin, {
+      const response = await fetch("https://striveschool-api.herokuapp.com/api/comments/" + props.asin, {
         headers: {
           Authorization:
             "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NjZiZjk3OTdjMjM5YzAwMTUyZjRiM2IiLCJpYXQiOjE3MTk0ODcxNTcsImV4cCI6MTcyMDY5Njc1N30.cnWiTJ8Skk8km6KUyP5pIXFi42u-vAP3LJVqlx_JKd8",
         },
       });
       if (response.ok) {
-        const comments = await response.json();
-        this.setState({ comments: comments });
+        const result = await response.json();
+        setComments(result);
         // console.log(comments);
       } else {
         throw new Error("Errore nel recupero dei commenti");
@@ -30,28 +33,18 @@ class CommentArea extends Component {
     }
   };
 
-  componentDidUpdate(prevProps) {
-    if (prevProps.asin !== this.props.asin) {
-      console.log("asin changed");
-      this.fetchComments();
-    } else {
-      console.log("no asin changed");
-    }
-  }
+  useEffect(() => {
+    fetchComments();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [props.asin]);
 
-  componentDidMount() {
-    // this.fetchComments();
-  }
-
-  render() {
-    console.log(this.props.asin);
-    return (
-      <>
-        <h2 className="mt-2">Commenti</h2>
-        {this.state.comments.length > 0 ? <CommentList comments={this.state.comments} /> : <Alert> Non ci sono commenti</Alert>}
-        {this.props.asin.length > 0 && <AddComment asin={this.props.asin} />}
-      </>
-    );
-  }
-}
+  console.log(props.asin);
+  return (
+    <>
+      <h2 className="mt-2">Commenti</h2>
+      {comments.length > 0 ? <CommentList comments={comments} /> : <Alert> Non ci sono commenti</Alert>}
+      {props.asin.length > 0 && <AddComment asin={props.asin} />}
+    </>
+  );
+};
 export default CommentArea;
