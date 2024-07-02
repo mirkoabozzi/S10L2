fbc5d6c48f0c807c4f7648ed3d8831e2e0bef694
Mirko Abozzi <mirko.abozzi@gmail.com>
2024-07-02 14:06:04 +0200

    refactor BookList

--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -1,42 +1,37 @@
 import { Col, Container, Form, InputGroup, Row } from "react-bootstrap";
 import SingleBook from "./SingleBook";
-import { Component } from "react";
 import CommentArea from "./CommentArea";
+import { useState } from "react";
 
-class BookList extends Component {
-  state = {
-    userBook: "",
-    selected: false,
-    asin: "",
+const BookList = (props) => {
+  const [userBook, setUserBook] = useState("");
+  const [asin, setAsin] = useState("");
+
+  const changeAsin = (newAsin) => {
+    setAsin(newAsin);
   };
 
-  changeAsin = (newAsin) => {
-    this.setState({ asin: newAsin });
-  };
-
-  render() {
-    // console.log(this.state.asin);
-    return (
-      <Row>
-        <Col sm={12} md={6}>
-          <Container>
-            <InputGroup className="my-3">
-              <Form.Control type="text" placeholder="Cerca un libro" aria-describedby="basic-addon2" value={this.state.userBook} onChange={(e) => this.setState({ userBook: e.target.value })} />
-            </InputGroup>
-            <Row>
-              {this.props.books
-                .filter((book) => book.title.toLowerCase().includes(this.state.userBook))
-                .map((book) => (
-                  <SingleBook key={book.asin} book={book} changeAsin={this.changeAsin} asin={this.state.asin} />
-                ))}
-            </Row>
-          </Container>
-        </Col>
-        <Col sm={12} md={6}>
-          <CommentArea asin={this.state.asin} />
-        </Col>
-      </Row>
-    );
-  }
-}
+  // console.log(this.state.asin);
+  return (
+    <Row>
+      <Col sm={12} md={6}>
+        <Container>
+          <InputGroup className="my-3">
+            <Form.Control type="text" placeholder="Cerca un libro" aria-describedby="basic-addon2" value={userBook} onChange={(e) => setUserBook(e.target.value)} />
+          </InputGroup>
+          <Row>
+            {props.books
+              .filter((book) => book.title.toLowerCase().includes(userBook))
+              .map((book) => (
+                <SingleBook key={book.asin} book={book} changeAsin={changeAsin} asin={asin} />
+              ))}
+          </Row>
+        </Container>
+      </Col>
+      <Col sm={12} md={6}>
+        <CommentArea asin={asin} />
+      </Col>
+    </Row>
+  );
+};
 export default BookList;
--- src/components/SingleBook.jsx
+++ src/components/SingleBook.jsx
@@ -1,21 +1,21 @@
 import { Button, Card, Col } from "react-bootstrap";
 
-const SingleBook = () => {
+const SingleBook = (props) => {
   return (
-    <Col key={this.props.book.asin} sm="12" md="6" lg="4" xxl="3" className="my-3">
+    <Col key={props.book.asin} sm="12" md="6" lg="4" xxl="3" className="my-3">
       <Card
         className="my-3 shadow"
         onClick={() => {
-          this.props.changeAsin(this.props.book.asin);
+          props.changeAsin(props.book.asin);
         }}
-        style={{ border: this.props.book.asin === this.props.asin ? "3px solid blue" : "3px solid transparent" }}
+        style={{ border: props.book.asin === props.asin ? "3px solid blue" : "3px solid transparent" }}
       >
-        <Card.Img variant="top" style={{ aspectRatio: 3 / 4 }} src={this.props.book.img} />
+        <Card.Img variant="top" style={{ aspectRatio: 3 / 4 }} src={props.book.img} />
         <Card.Body>
-          <Card.Title className="text-truncate">{this.props.book.title}</Card.Title>
-          <Card.Text>{this.props.book.price} €</Card.Text>
+          <Card.Title className="text-truncate">{props.book.title}</Card.Title>
+          <Card.Text>{props.book.price} €</Card.Text>
 
-          <Button variant={this.props.book.asin === this.props.asin ? "secondary" : "primary"}>Compra ora</Button>
+          <Button variant={props.book.asin === props.asin ? "secondary" : "primary"}>Compra ora</Button>
         </Card.Body>
       </Card>
     </Col>
